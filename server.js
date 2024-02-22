761d1b0aad01710ffd95014261cbcfebb226a6bd
maximshilo00 <maximshilo00@gmail.com>
2024-02-22 17:32:47 +0200

    added mongo connectivity

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const PORT = process.env.PORT || 4000
 
 app.use(express.static('./client/build'))
+mongoose.connect('mongodb+srv://maximshilo00:326974359max@projectdb.j3ljufi.mongodb.net/echo_collection')
+
+const userSchema = mongoose.Schema({
+    nickname : String,
+    username : String,
+    password : String,
+    following : []
+})
+
+const userModel = mongoose.model('users', userSchema)
+
 
 app.get('*', (req, res) => {
     res.sendFile('./index.html')
